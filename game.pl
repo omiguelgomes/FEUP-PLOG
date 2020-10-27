be86e0617fc899ad892c7926a4a52fb54428e67f
omiguelgomes <miguelrgomes1698@gmail.com>
2020-10-27 16:52:26 +0000

    Initial board shown

--- game.pl
+++ game.pl
@@ -1,22 +1,31 @@
 display_game(GameState, Player) :- printBoard(GameState).
 
-initial([[w, w, w, w, w, w, w, w, w, w],
-         [w, o, o, o, o, o, o, o, o, w],
-         [w, o, o, o, o, o, o, o, o, w],
-         [w, o, o, o, o, o, o, o, o, w],
-         [w, o, o, o, o, o, o, o, o, w],
-         [w, o, o, o, o, o, o, o, o, w],
-         [w, o, o, o, o, o, o, o, o, w],
-         [w, o, o, o, o, o, o, o, o, w],
-         [w, o, o, o, o, o, o, o, o, w],
-         [w, w, w, w, w, w, w, w, w, w]]).
+%INITIAL BOARD
+initial([[#, #, #, #, #, #, #, #, #, #],
+         [#, o, o, o, o, o, o, o, o, #],
+         [#, o, o, o, o, o, o, o, o, #],
+         [#, o, o, o, o, o, o, o, o, #],
+         [#, o, o, o, 'W', 'B', o, o, o, #],
+         [#, o, o, o, 'B', 'W', o, o, o, #],
+         [#, o, o, o, o, o, o, o, o, #],
+         [#, o, o, o, o, o, o, o, o, #],
+         [#, o, o, o, o, o, o, o, o, #],
+         [#, #, #, #, #, #, #, #, #, #]]).
 
-printBoard([H|T]) :- write(' __________________________________________'),nl,
+%prints board, row by row
+
+printBoard([H|T]) :- write(' -----------------------------------------'),
+                     nl,
                      printRow(H),
+                     nl,
                      printBoard(T).
-printBoard([]) :-    write(' _________________________________________ '),nl.
 
+printBoard([]) :-    write(' -----------------------------------------'), nl.
+
+%prints row, cell by cell
+
+printRow(['o'|T]) :- write(' | '), write(' '), printRow(T). %special case, doesnt print anything in an empty cell
 printRow([H|T]) :- write(' | '), write(H), printRow(T). 
-printRow([]) :- write(' |'), nl.
+printRow([]) :- write(' |').
 
 %reconsult('game.pl').
