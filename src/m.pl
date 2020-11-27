70eddf3fffc1d05982a432ba1e60ee6d8f841857
Pedro <pedromatt97@hotmail.com>
2020-11-27 04:43:06 +0000

    erros estupidos, está tenso

--- src/m.pl
+++ src/m.pl
@@ -1,5 +1,7 @@
 /*make it quicker to reload proj*/
+:- use_module(library(lists)).
 :- include('menu.pl').
 :- include('game.pl').
 :- include('display.pl').
 :- include('utils.pl').
+:- include('testar.pl').
