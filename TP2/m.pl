b6d3ab0889130a887c7e3046a5d9a492554c2a99
Pedro <pedromatt97@hotmail.com>
2020-12-31 18:01:40 +0000

    gui added

--- TP2/m.pl
+++ TP2/m.pl
@@ -2,5 +2,7 @@
 :- consult('menu.pl').
 :- consult('utils.pl').
 :- consult('puzzle.pl').
+:- consult('teste.pl').
 :- use_module(library(random)).
 :- use_module(library(clpfd)).
+:- use_module(library(lists)).
