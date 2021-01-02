f238a33e7ac5b942d07d4bf0ec5bfdf761031b8b
Pedro <pedromatt97@hotmail.com>
2021-01-02 01:20:54 +0000

    still need to diferentiate squares

--- TP2/teste.pl
+++ TP2/teste.pl
@@ -1,8 +1,11 @@
 omega:-
-    generateRandomGrid(Height-Width, Diamonds),
-    generateBoard(Height,Width,Board),
-    write('Height: '), write(Height), nl,
+    generateBoard(9,9,Board),
+   /* write('Height: '), write(Height), nl,
     write('Width: '), write(Width), nl,  
     write(Diamonds),nl,
-    fillDiamonds(Board, Diamonds, NewBoard, Final),
-    printBoard(Final, 0).
+    fillDiamonds(Board, Diamonds, NewBoard, Final), */
+    printBoard(Board, 0),
+    nl, nl, write('testing starts here'), nl, nl,
+    makeAllSquares(Board, [(0-0,2),(3-3,2), (5-5,4)], '1', FinalBoard),
+    printBoard(FinalBoard, 0).
+
