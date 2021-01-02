2d8627901bdda3d028d70679e0156061c8606b2e
omiguelgomes <miguelrgomes1698@gmail.com>
2021-01-02 01:39:46 +0000

    deleted trash

--- TP2 (copy)/m.pl
+++
@@ -1,8 +0,0 @@
-:- use_module(library(clpfd)).
-:- use_module(library(random)).
-:- use_module(library(lists)).
-:- consult('display.pl').
-:- consult('menu.pl').
-:- consult('utils.pl').
-:- consult('puzzle.pl').
-:- consult('teste.pl').
